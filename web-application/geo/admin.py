28717184fc503a48ee4c211f6cdb8dcf89f35631
Nikita Koval <me@nikitakoval.com>
2015-07-31 11:18:31 +0600

    #82 Создал фикстуры с данными. Зарегистрировал модели в админке.

--- web-application/geo/admin.py
+++ web-application/geo/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from geo.models import Region, District
 
-# Register your models here.
+
+class DistrictAdmin(admin.ModelAdmin):
+    list_display = ['title', 'region']
+
+
+admin.site.register(Region)
+admin.site.register(District, DistrictAdmin)
--- web-application/geo/models.py
+++ web-application/geo/models.py
@@ -5,7 +5,13 @@ from django.utils.translation import ugettext_lazy as _
 class Region(models.Model):
     title = models.CharField(max_length=100, null=False, blank=False, unique=True, verbose_name=_("Title"))
 
+    def __unicode__(self):
+        return self.title
+
 
 class District(models.Model):
     title = models.CharField(max_length=100, null=False, blank=False, unique=True, verbose_name=_("Title"))
     region = models.ForeignKey(Region)
+
+    def __unicode__(self):
+        return self.title
