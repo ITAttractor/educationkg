82d04a5b50bdf0798f37b1b12facd4055dd6802f
Nikita Koval <me@nikitakoval.com>
2015-08-18 10:45:55 +0600

    #101 Создал добавил поле в модель, добавил миграции, обновил фикстуры

--- web-application/geo/models.py
+++ web-application/geo/models.py
@@ -4,6 +4,7 @@ from django.utils.translation import ugettext_lazy as _
 
 class Region(models.Model):
     title = models.CharField(max_length=100, null=False, blank=False, unique=True, verbose_name=_("Title"))
+    slug = models.CharField(max_length=20, null=True, blank=True, unique=True)
 
     def __unicode__(self):
         return self.title
